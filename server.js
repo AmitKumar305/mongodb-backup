a8dc4bc840d94d0ccc9672494bd735b32fed3ebe
Amit Kumar <65401586+AmitKumar305@users.noreply.github.com>
2023-12-30 12:06:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const { CronJob } = require('cron');
 
 const mongoExportFileUpload = require('./mongoExportFileUpload');
-const gzBufferUpload = require('./gzBufferUpload');
+// const gzBufferUpload = require('./gzBufferUpload');
 
 
 new CronJob('*/1 * * * *', () => {
     // gzBufferUpload();
 	// console.log('Mongo GZ File Upload');
     mongoExportFileUpload();
-	console.log('Mongo Export File Upload');
+	console.log('Mongo Export File Upload ......');
 }, null, true, 'America/Los_Angeles');
