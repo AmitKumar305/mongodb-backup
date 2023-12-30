d0275b72fa327f98eac03c9264c7213e352877a6
Amit Kumar <65401586+AmitKumar305@users.noreply.github.com>
2023-12-30 12:02:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const gzBufferUpload = require('./gzBufferUpload');
 
 
 new CronJob('*/1 * * * *', () => {
-    gzBufferUpload();
-	console.log('Mongo GZ File Upload');
-    // mongoExportFileUpload();
-	// console.log('Mongo Export File Upload');
+    // gzBufferUpload();
+	// console.log('Mongo GZ File Upload');
+    mongoExportFileUpload();
+	console.log('Mongo Export File Upload');
 }, null, true, 'America/Los_Angeles');
