9f8939adc62448031bc82215e63b920841e102c1
Amit Kumar <65401586+AmitKumar305@users.noreply.github.com>
2023-12-31 16:03:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const { CronJob } = require('cron');
 require('dotenv').config({ path: '/home/ubuntu/.env' });
 const mongoExportFileUpload = require('./mongoExportFileUpload');
-// const gzBufferUpload = require('./gzBufferUpload');
+const gzBufferUpload = require('./gzBufferUpload');
 
 
 new CronJob('*/1 * * * *', () => {
-    // gzBufferUpload();
-	// console.log('Mongo GZ File Upload');
+    gzBufferUpload();
+	console.log('Mongo GZ File Upload');
     mongoExportFileUpload();
 	console.log('Mongo Export File Upload ......');
-	console.log(process.env.BUCKET_NAME);
 }, null, true, 'America/Los_Angeles');
